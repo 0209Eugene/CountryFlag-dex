5716a130b20e9f83e78b59731c20763c70e7ba0d
Yuto Miura <veda.ym0209@gmail.com>
2020-01-02 11:31:48 +0900

    Updates

---
+++ precache-manifest.6d2f954553bffc43ae32bc92df595cf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2801fda91a398815ab5d27eaed2c79b7",
+    "url": "/CountryFlag-dex/index.html"
+  },
+  {
+    "revision": "484eb33f1cb895e604ba",
+    "url": "/CountryFlag-dex/static/css/main.4811a178.chunk.css"
+  },
+  {
+    "revision": "83467782d8a629629571",
+    "url": "/CountryFlag-dex/static/js/2.e03828f1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/CountryFlag-dex/static/js/2.e03828f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "484eb33f1cb895e604ba",
+    "url": "/CountryFlag-dex/static/js/main.125780bb.chunk.js"
+  },
+  {
+    "revision": "873859beed9c30a68d70",
+    "url": "/CountryFlag-dex/static/js/runtime-main.5350bac8.js"
+  }
+]);
